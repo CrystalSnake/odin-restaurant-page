7a064e38b0d55862ba45c33912948234eadac8e1
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-01-03 12:28:05 +0400

    Add header and background

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './style.css';
 
 console.log('Hello world!');
+
+const container = document.createElement('div');
+container.setAttribute('id', 'content');
+
+const header = document.createElement('header');
+const logo = document.createElement('h1');
+logo.textContent = 'Asian Restaurant';
+const tabMenu = document.createElement('div');
+const ul = document.createElement('ul');
+const home = document.createElement('li');
+home.textContent = 'Home';
+const menu = document.createElement('li');
+menu.textContent = 'Menu';
+const contacts = document.createElement('li');
+contacts.textContent = 'Contacts';
+ul.appendChild(home);
+ul.appendChild(menu);
+ul.appendChild(contacts);
+tabMenu.appendChild(ul);
+header.appendChild(logo);
+header.appendChild(tabMenu);
+
+container.appendChild(header);
+
+document.body.appendChild(container);
