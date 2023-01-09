9f7e1b3e53eee8f1ae1c8892dd9f7c6563370264
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2023-01-09 21:41:12 +0400

    Add home tab event listener

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
 import Promo from './images/nature-white-restaurant-dish-meal-food-760717-pxhere.com.jpg';
 
 function getHome() {
-  const mainContent = document.createElement('div');
-  mainContent.classList.add('main-content');
+  const homeContent = document.createElement('div');
+  homeContent.classList.add('main-content');
   const about = document.createElement('p');
   about.textContent =
     'At the hikari restaurant, diners can enjoy not only our exquisite range of dishes, but also a cozy atmosphere and caring service. And if it is not possible to visit the restaurant, then we will deliver your dishes to your home or office.';
@@ -11,11 +11,11 @@ function getHome() {
   promo.src = Promo;
   const aboutOpen = document.createElement('p');
   aboutOpen.textContent = 'Working for you since 2010.';
-  mainContent.appendChild(about);
-  mainContent.appendChild(promo);
-  mainContent.appendChild(aboutOpen);
+  homeContent.appendChild(about);
+  homeContent.appendChild(promo);
+  homeContent.appendChild(aboutOpen);
 
-  return mainContent;
+  return homeContent;
 }
 
 export default getHome;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import mainContent from './home';
+import homeContent from './home';
 import './style.css';
 
 import '@fortawesome/fontawesome-free/js/brands';
@@ -12,14 +12,22 @@ container.setAttribute('id', 'content');
 const header = document.createElement('header');
 const logo = document.createElement('h1');
 logo.textContent = 'Hikari';
+
 const tabMenu = document.createElement('div');
 const ul = document.createElement('ul');
 const home = document.createElement('li');
 home.textContent = 'Home';
+home.addEventListener('click', (e) => {
+  loadMain(homeContent());
+});
 const menu = document.createElement('li');
 menu.textContent = 'Menu';
+menu.addEventListener('click', (e) => {
+  loadMain();
+});
 const contacts = document.createElement('li');
 contacts.textContent = 'Contacts';
+
 ul.appendChild(home);
 ul.appendChild(menu);
 ul.appendChild(contacts);
@@ -31,19 +39,30 @@ container.appendChild(header);
 
 //Main section
 const main = document.createElement('main');
-main.textContent = '';
-main.appendChild(mainContent());
+
+function loadMain(content) {
+  const main = document.querySelector('main');
+  main.textContent = '';
+  main.appendChild(content);
+}
+
 container.appendChild(main);
 
 //Footer section
-const footer = document.createElement('footer');
-const copy = document.createElement('p');
-copy.textContent = 'Copyright © 2023 CrystalSnake';
-const githubLink = document.createElement('a');
-githubLink.href = 'https://github.com/CrystalSnake';
-githubLink.innerHTML = '<i class="fa-brands fa-github"></i>';
-footer.appendChild(copy);
-footer.appendChild(githubLink);
-container.appendChild(footer);
+function getFooter() {
+  const footer = document.createElement('footer');
+  const copy = document.createElement('p');
+  copy.textContent = 'Copyright © 2023 CrystalSnake';
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/CrystalSnake';
+  githubLink.innerHTML = '<i class="fa-brands fa-github"></i>';
+  footer.appendChild(copy);
+  footer.appendChild(githubLink);
+  container.appendChild(footer);
+}
+
+getFooter();
 
 document.body.appendChild(container);
+
+loadMain(homeContent());
